96a03342545376ec1fe7282dc65b8d8cce98d2c8
Sina <sadeghesfahani.sina@gmail.com>
2023-04-20 09:03:53 +0430

    Question 4 implemented
    custom MyLinkList implemented

---
+++ src/MyLinkedList.java
@@ -0,0 +1,115 @@
+public class MyLinkedList <T>{
+
+    private Node<T> head;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(T value) {
+        Node<T> newNode = new Node<>(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(newNode);
+        }
+        size++;
+    }
+
+    public void add(int index, T value) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<T> newNode = new Node<>(value);
+        if (index == 0) {
+            newNode.setNext(head);
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.getNext();
+            }
+            newNode.setNext(current.getNext());
+            current.setNext(newNode);
+        }
+        size++;
+    }
+
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            head = head.getNext();
+        } else {
+            Node<T> current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.getNext();
+            }
+            current.setNext(current.getNext().getNext());
+        }
+        size--;
+    }
+
+    public void remove(T value) {
+        if (head == null) {
+            return;
+        }
+        if (head.getValue().equals(value)) {
+            head = head.getNext();
+            size--;
+            return;
+        }
+        Node<T> current = head;
+        while (current.getNext() != null) {
+            if (current.getNext().getValue().equals(value)) {
+                current.setNext(current.getNext().getNext());
+                size--;
+                return;
+            }
+            current = current.getNext();
+        }
+    }
+
+    public int indexOf(T value) {
+        Node<T> current = head;
+        for (int i = 0; i < size; i++) {
+            if (current.getValue().equals(value)) {
+                return i;
+            }
+            current = current.getNext();
+        }
+        return -1;
+    }
+    private static class Node<T> {
+        private T value;
+        private Node<T> next;
+
+        public Node(T value) {
+            this.value = value;
+            next = null;
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+    }
+}
